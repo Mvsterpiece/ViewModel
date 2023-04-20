3170fc41f0fb56a3a9030472bdbe775325b6a358
opilane <opilane@HP-CZC2349HTQ>
2023-04-20 09:47:53 +0300

    Add project files.

---
+++ ViewModel/ViewModel/ViewModels/FriendViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using ViewModel.Models;
+
+namespace ViewModel.ViewModels
+{
+    public class FriendViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        FriendsListViewModel lvm;
+
+        public Friend Friend { get; private set; }
+
+        public FriendViewModel()
+        {
+            Friend = new Friend();
+        }
+
+
+        public FriendsListViewModel ListViewModel
+        {
+            get { return lvm; }
+            set
+            {
+                if (lvm != value)
+                {
+                    lvm = value;
+                    OnPropertyChanged("ListViewModel");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return Friend.Name; }
+            set
+            {
+                if (Friend.Name != value)
+                {
+                    Friend.Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get { return Friend.Email; }
+            set
+            {
+                if (Friend.Email != value)
+                {
+                    Friend.Email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+
+        public string Phone
+        {
+            get { return Friend.Phone; }
+            set
+            {
+                if (Friend.Phone != value)
+                {
+                    Friend.Phone = value;
+                    OnPropertyChanged("Phone");
+                }
+            }
+        }
+
+
+        public bool Isvalid
+        {
+            get
+            {
+                return ((!string.IsNullOrEmpty(Name.Trim()))) ||
+                    (!string.IsNullOrEmpty(Phone.Trim())) ||
+                    (!string.IsNullOrEmpty(Email.Trim()));
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ ViewModel/ViewModel/ViewModels/FriendsListViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using ViewModel.Views;
+using Xamarin.Forms;
+
+namespace ViewModel.ViewModels
+{
+    public class FriendsListViewModel
+    {
+        public ObservableCollection<FriendViewModel> Friends { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand CreateFriendCommand { get; set; }
+        public ICommand DeleteFriendCommand { get; set; }
+        public ICommand SaveFriendCommand { get; set; }
+        public ICommand BackCommand { get; set; }
+        FriendViewModel selectedFriend;
+        public INavigation Navigation { get; set; }
+
+
+        public FriendsListViewModel() 
+        {
+            Friends = new ObservableCollection<FriendViewModel>();
+            CreateFriendCommand = new Command(CreateFriend);
+            DeleteFriendCommand = new Command(DeleteFriend);
+            SaveFriendCommand = new Command(SaveFriend);
+            BackCommand = new Command(Back);
+        }
+
+        private void Back()
+        {
+            Navigation.PopAsync();
+        }
+
+        private void SaveFriend(object friendObject)
+        {
+            FriendViewModel friend = friendObject as FriendViewModel;
+            if (friend != null && friend.Isvalid && !Friends.Contains(friend))
+            {
+                Friends.Add(friend);
+            }
+            Back();
+        }
+
+        private void CreateFriend()
+        {
+            Navigation.PopAsync(new FriendPage( new FriendViewModel() { ListViewModel = this }));
+        }
+
+        public FriendViewModel SelectedFriend
+        {
+            get { return selectedFriend; }
+            set
+            { 
+                if(selectedFriend != value)
+                {
+                    FriendViewModel tempFriend = value;
+                    selectedFriend = value;
+                    OnPropertyChanged("SelectedFriend");
+                    Navigation.PushAsync(new FriendPage(tempFriend));
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private void DeleteFriend(object friendObject)
+        {
+            FriendViewModel friend = friendObject as FriendViewModel;
+            if (friend != null)
+            {
+                Friends.Remove(friend);
+            }
+            Back();
+        }
+
+    }
+}
+
